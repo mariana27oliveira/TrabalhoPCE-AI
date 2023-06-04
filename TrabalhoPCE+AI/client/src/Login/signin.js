a776b1cc1e5c9721779f5689054fc89a171efa15
mariana27oliveira <marianarodriguesoliveira2001@gmail.com>
2023-06-04 17:40:16 +0100

    04.06

---
+++ TrabalhoPCE+AI/client/src/Login/signin.js
@@ -0,0 +1,75 @@
+import './login.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SigninForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+      return;
+    }
+    try {
+      const response = await axios.post('http://localhost:8080/signIn', {
+        email: email,
+        password: password
+      });
+      if (response.data.success) {
+        setMessage('Account created successfully');
+        navigate('/');
+      } else {
+        setMessage(response.data.info);
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('An error occurred');
+    }
+  };
+
+  return (
+    <div className="signup-wrapper">
+      <div className="signup-header">
+        <h1>Create an Account</h1>
+      </div>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Enter your name" required value={name} onChange={handleNameChange} />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Enter your email" required value={email} onChange={handleEmailChange} />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Enter your password" required value={password} onChange={handlePasswordChange} />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm your password" required value={confirmPassword} onChange={handleConfirmPasswordChange} />
+        <button type="submit">Create Account</button>
+        <hr></hr>
+        {message && <div>{message}</div>}
+      </form>
+    </div>
+  );
+}
+
+export default SigninForm;
