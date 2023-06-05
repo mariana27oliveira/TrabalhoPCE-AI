8946af01d7c9f4d3802a8511e4f9efb1e6e21a88
bileite4 <bileite_4@hotmail.com>
2023-06-05 14:46:21 +0100

    .

--- TrabalhoPCE+AI/server/routes/routesFile.js
+++ TrabalhoPCE+AI/server/routes/routesFile.js
@@ -141,7 +141,7 @@ router.get("/mensagem/:id", async (req, res) => {
     identificador= req.params.id;
     let deletemsg = await messageFHIRController.deleteMensagem(identificador);
     let deletecomposition = await CompositionController.deleteComposition(identificador);
-    if(!deletecomposition.success) throw "Erro a eliminar a mensagem FHIR";
+    if(!deletecomposition.success | !deletemsg.success) throw "Erro a eliminar a mensagem FHIR";
     else
       res.status(200).json({ success: true, info: "Mensagem apagada com sucesso"});
     });
